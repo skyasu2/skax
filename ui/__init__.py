216d83fac0bcfb8e6ae83c43a960fbbf5fa1cdc1
skyasu2 <skyasu2@gmail.com>
2026-01-05 00:05:19 +0900

    Fix: Resolve circular import by clearing ui/__init__.py

--- ui/__init__.py
+++ ui/__init__.py
@@ -2,56 +2,5 @@
 UI Package
 
 PlanCraft Agent의 UI 컴포넌트들을 모듈화합니다.
+Direct imports are recommended to avoid circular dependencies.
 """
-
-from ui.dynamic_form import render_pydantic_form
-
-# Mermaid 다이어그램 모듈 (분리됨)
-from ui.mermaid import (
-    render_mermaid,
-    render_scalable_mermaid,
-    render_markdown_with_mermaid
-)
-
-from ui.components import (
-    render_progress_steps,
-    render_timeline,
-    render_chat_message,
-    render_error_state,
-    render_option_selector,
-    render_visual_timeline,
-    render_human_interaction,
-    render_specialist_agents_status
-)
-
-from ui.dialogs import (
-    show_plan_dialog,
-    show_analysis_dialog,
-    show_history_dialog,
-    render_dev_tools
-)
-
-from ui.refinement import render_refinement_ui
-
-__all__ = [
-    # Mermaid
-    "render_mermaid",
-    "render_scalable_mermaid",
-    "render_markdown_with_mermaid",
-    # Components
-    "render_progress_steps",
-    "render_timeline", 
-    "render_chat_message",
-    "render_error_state",
-    "render_option_selector",
-    "render_visual_timeline",
-    "render_human_interaction",
-    "render_specialist_agents_status",
-    # Dialogs
-    "show_plan_dialog",
-    "show_analysis_dialog",
-    "show_history_dialog",
-    "render_dev_tools",
-    # Refinement
-    "render_refinement_ui"
-]
